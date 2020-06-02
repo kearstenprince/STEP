fea1934a649d84c302179c4c602a23b787cd93aa
Kearsten Prince <kearstenprince@gmail.com>
2020-06-02 16:07:23 +0000

    starting portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['Hasta La Vista, baby', '¡Say Hello to my little friend!', 'Im Bond. James Bond.' , 'Bonjour le monde!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
